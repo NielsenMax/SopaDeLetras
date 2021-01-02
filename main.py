ef69b49f33c00e6b0a48066745aee640cd23e3c7
NielsenMax <maxianielsen@gmail.com>
2021-01-02 20:45:44 -0300

    una banda

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 def checkearPalabra(tablero, palabra, orientacion, i, j):
     largoP = len(palabra)
     bandera = True
@@ -6,8 +7,7 @@ def checkearPalabra(tablero, palabra, orientacion, i, j):
                 bandera = False
                 break
     return bandera
-
-def ubicacionDisponible(tablero, palabra, orientacion):
+def ubicacionDisponible(tablero, palabra, orientacion, yaUsadas):
     dimension = len(tablero)
     largoP= len(palabra)
     I = -1
@@ -22,8 +22,7 @@ def ubicacionDisponible(tablero, palabra, orientacion):
         maxJ = dimension - ((largoP - 1) * orientacion[1])
         for i in range(inicioI, maxI):
             for j in range(maxJ):
-                if(tablero[i][j] == ' ' or tablero[i][j] == palabra[0]):
-                    print(checkearPalabra(tablero, palabra, orientacion,i, j))
+                if((tablero[i][j] == ' ' or tablero[i][j] == palabra[0]) and not (i,j) in yaUsadas):
                     if(checkearPalabra(tablero, palabra, orientacion,i, j)):
                         I = i
                         J = j
@@ -33,11 +32,39 @@ def ubicacionDisponible(tablero, palabra, orientacion):
     return (I,J)
 
 def insertarPalabra(tablero, palabra, orientacion, i, j):
+    Tablero = deepcopy(tablero)
     largoP = len(palabra)
     for k in range(largoP):
-        tablero[i + k * orientacion[0]][j + k * orientacion[1]] = palabra[k]
-    return tablero
+        Tablero[i + (k * orientacion[0])][j + (k * orientacion[1])] = palabra[k]
+    return Tablero
 
-tablero = [['a','h','h','t','r','a',],['r','h',' ','b',' ',' ',],[' ',' ',' ',' ',' ',' ',],[' ',' ',' ',' ',' ',' ',],[' ',' ',' ',' ',' ',' ',],[' ',' ',' ',' ',' ',' ',]]
-p = ubicacionDisponible(tablero, "hola", (-1,1))
-print(insertarPalabra(tablero, "hola", (-1,1), p[0], p[1]))
+def generarTableroDePalabras(tablero, palabras):
+    Tablero = deepcopy(tablero)
+    print('------------------temp')
+    imprimirTablero(tablero)
+    if(palabras):
+        yaUsadas = []
+        posible = ubicacionDisponible(Tablero, palabras[0][0], palabras[0][1], yaUsadas)
+        while(posible != (-1,-1)):
+            tableroTemp = insertarPalabra(Tablero, palabras[0][0], palabras[0][1], posible[0], posible[1])
+            tableroTemp2 = generarTableroDePalabras(tableroTemp, palabras[1:])
+            #print('------------------temp')
+            #imprimirTablero(tableroTemp)
+            #print('------------------temp2')
+            #imprimirTablero(tableroTemp2)
+            if(tableroTemp2 == tableroTemp):
+                yaUsadas += [posible]
+                posible = ubicacionDisponible(tablero, palabras[0][0], palabras[0][1], yaUsadas)
+            else:
+                Tablero = deepcopy(tableroTemp2)
+                posible = (-1,-1)
+    #imprimirTablero(tablero)
+    return Tablero
+
+def imprimirTablero(tablero):
+    print('\n'.join(map((lambda x: ''.join(x)), tablero)))
+
+
+t = [[' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ']]
+palabras = [('hola', (1,1)),('casa', (0,1)),('linda', (1,1)),('maxi', (-1,1)),('gato', (1,0))]
+imprimirTablero(generarTableroDePalabras(deepcopy(t), palabras))
