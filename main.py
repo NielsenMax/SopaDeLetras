0f49bc8481e701a5b76afb1be69ad9d687389782
NielsenMax <maxianielsen@gmail.com>
2021-01-05 19:20:35 -0300

    Terminada funciones principales

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from copy import deepcopy
+import random
+
 def checkearPalabra(tablero, palabra, orientacion, i, j):
     largoP = len(palabra)
     bandera = True
@@ -7,6 +8,7 @@ def checkearPalabra(tablero, palabra, orientacion, i, j):
                 bandera = False
                 break
     return bandera
+
 def ubicacionDisponible(tablero, palabra, orientacion, yaUsadas):
     dimension = len(tablero)
     largoP= len(palabra)
@@ -32,39 +34,115 @@ def ubicacionDisponible(tablero, palabra, orientacion, yaUsadas):
     return (I,J)
 
 def insertarPalabra(tablero, palabra, orientacion, i, j):
-    Tablero = deepcopy(tablero)
     largoP = len(palabra)
     for k in range(largoP):
-        Tablero[i + (k * orientacion[0])][j + (k * orientacion[1])] = palabra[k]
-    return Tablero
+        tablero[i + (k * orientacion[0])][j + (k * orientacion[1])] = palabra[k]
+    return tablero
+
+def eliminarPalabra(tablero, palabra, orientacion, posicion):
+    largoP = len(palabra)
+    i = posicion[0]
+    j = posicion[1]
+    for k in range(largoP):
+        if(tablero[i + (k * orientacion[0])][j + (k * orientacion[1])] == palabra[k]):
+            tablero[i + (k * orientacion[0])][j + (k * orientacion[1])] = ' '
+    return tablero
 
 def generarTableroDePalabras(tablero, palabras):
-    Tablero = deepcopy(tablero)
-    print('------------------temp')
-    imprimirTablero(tablero)
-    if(palabras):
-        yaUsadas = []
-        posible = ubicacionDisponible(Tablero, palabras[0][0], palabras[0][1], yaUsadas)
-        while(posible != (-1,-1)):
-            tableroTemp = insertarPalabra(Tablero, palabras[0][0], palabras[0][1], posible[0], posible[1])
-            tableroTemp2 = generarTableroDePalabras(tableroTemp, palabras[1:])
-            #print('------------------temp')
-            #imprimirTablero(tableroTemp)
-            #print('------------------temp2')
-            #imprimirTablero(tableroTemp2)
-            if(tableroTemp2 == tableroTemp):
-                yaUsadas += [posible]
-                posible = ubicacionDisponible(tablero, palabras[0][0], palabras[0][1], yaUsadas)
+    yaUsadas = {}
+    for palabra in palabras:
+        yaUsadas[palabra[0]] = []
+    i = 0
+    while( i < len(palabras)):
+        posible = ubicacionDisponible(tablero, palabras[i][0], palabras[i][1], yaUsadas[palabras[i][0]])
+        if(posible == (-1,-1)):
+            i -= 1
+            if(i == -1):
+                break
             else:
-                Tablero = deepcopy(tableroTemp2)
-                posible = (-1,-1)
-    #imprimirTablero(tablero)
-    return Tablero
+                tablero = eliminarPalabra(tablero, palabras[i][0], palabras[i][1],yaUsadas[palabras[i][0]][-1])
+                i -= 1
+        else:
+            tablero = insertarPalabra(tablero,palabras[i][0], palabras[i][1], posible[0],posible[1])
+            yaUsadas[palabras[i][0]] += [posible]
+        i += 1
+    return tablero
+
+#TODO usar posicionesLetras para ver si anda
+def letraExtremo(letras, orientacion, sentido):
+    extremo = letras[0]
+    for v in letras[1:]:
+        
+
+def formaPalabra(tablero, palabra, orientacion, substring, posicionesLetras):
+    print(substring)
+    dimension = len(tablero)
+    retorno = False
+    if(substring == palabra):
+        retorno = True
+    elif(substring in palabra):
+        letraMaxDerecha = letraExtremo(posicionesLetras, orientacion, 1)
+        movIDer = letraMaxDerecha[0} + orientacion[0]
+        movJDer = letraMaxDerecha[1] + orientacion[1] 
+        if(movIDer > 0 and movIDer < dimension and movJDer > 0 and movJDer < dimension):
+            retornoDer = formaPalabra(tablero, palabra, orientacion, substring + tablero[movIDer][movJDer], posicionesLetras += [(movIDer,movJDer)])
+        else:
+            retornoDer = False
+        letraMaxIzquierda = letraExtremo(posicionesLetras, orientacion, -1)
+        movIIzq = letraMaxIzquierda[0] - orientacion[0]
+        movJIzq = letraMaxIzquierda[1] - orientacion[1] 
+        if(movIIzq > 0 and movIIzq < dimension and movJIzq > 0 and movJIzq < dimension):
+            retornoIzq = formaPalabra(tablero, palabra, orientacion, substring + tablero[movIIzq][movJIzq], posicionesLetras += [(movIIzq,movJIzq)])
+        else:
+            retornoIzq = False
+        retorno = retorno or retornoDer or retornoIzq
+    return retorno
+#TODO que anda la funcion anterior
+
+def letraValida(tablero, palabras, i, j):
+    palabrasPropias = []
+    dimension = len(tablero)
+    retorno = False 
+    for palabra in palabras:
+        if(tablero[i][j] in palabra[0]):
+            palabrasPropias += [palabra]
+    for palabra in palabrasPropias:
+        if(retorno):
+            break
+        movIDer = i + palabra[1][0]
+        movJDer = j + palabra[1][1]
+        if(movIDer >= 0 and movIDer < dimension and movJDer >= 0 and movJDer < dimension):
+            retornoDer = formaPalabra(tablero, palabra[0], palabra[1], tablero[i][j] + tablero[movIDer][movJDer])
+        else:
+            retornoDer = False
+        movIIzq = i - palabra[1][0]
+        movJIzq = j - palabra[1][1] 
+        if(movIIzq >= 0 and movIIzq < dimension and movJIzq >= 0 and movJIzq < dimension):
+            retornoIzq = formaPalabra(tablero, palabra[0], palabra[1], tablero[i][j] + tablero[movIIzq][movJIzq])
+        else:
+            retornoIzq = False
+        retorno = retornoDer or retornoIzq
+    return retorno
+        
+def rellenarVacios(tablero, palabras):
+    alfabeto = {"a","b","c","d","e","f","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
+    dimension = len(tablero)
+    for i in range(dimension):
+        for j in range(dimension):
+            if(tablero[i][j] == ' '):
+                insertarLetra = True
+            while(insertarLetra):    
+                tablero[i][j] = random.choice(tuple(alfabeto))
+                insertarLetra = not letraValida(tablero, palabras, i, j)
+    return tablero
 
 def imprimirTablero(tablero):
     print('\n'.join(map((lambda x: ''.join(x)), tablero)))
 
-
 t = [[' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ']]
-palabras = [('hola', (1,1)),('casa', (0,1)),('linda', (1,1)),('maxi', (-1,1)),('gato', (1,0))]
-imprimirTablero(generarTableroDePalabras(deepcopy(t), palabras))
+palabras = [('hola', (1,1)),('casa', (0,1)),('linda', (1,1)),('maxi', (-1,1)),('gato', (1,0)),('nico',(0,1))]
+imprimirTablero(generarTableroDePalabras(t, palabras))
+t[1][2] = 'z'
+print(letraValida(t, palabras, 0, 1))
+
+
